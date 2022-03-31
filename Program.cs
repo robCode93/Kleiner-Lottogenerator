ef1d983f7cf058acf7a4f07bae35ace8ea73204e
RobCoding <robertbrock93@yahoo.de>
2022-03-31 18:11:43 +0200

    Lotto Generator
    - Abfrage der Daten zur Berechnung (Preis pro Spiel/ Spiele Pro Woche)
    -Abfrage der Daten für Loop und Funktionen (Min- und Maxwert für Zufallszahlen + Tippzahlen)
    - Anlegen der Funktionen (Erstellen eines Arrays mit Speicherplätzen nach spielenden Zahlen) + (Füllen des Arrays für Generator mit Zufallszahlen) + (Prüfen ob Zahlen in Array vorkommen) + (Loop mit Rückgabe der Versuche bis all matched)

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using Zahlen_Raten;
+
+// Vars for the Programm
+int[] myNumbers;
+int start, end, counter, numberAdd;
+TestingClass tester = new TestingClass();
+decimal priceForOneTip;
+decimal spendingMoney;
+int weeks, months, years, perWeek;
+
+
+Console.WriteLine("---------------------------------------------");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Der ultimative Glücksspiel-Generator");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("---------------------------------------------");
+Console.WriteLine();
+Console.Write("Preis pro Tipp: ");
+priceForOneTip = Convert.ToDecimal(Console.ReadLine());
+Console.Write("Wie oft pro Woche wird gespielt: ");
+perWeek = Convert.ToInt32(Console.ReadLine());
+Console.Write("Welche Zahl soll als Minimum gesetzt werden: ");
+start = Convert.ToInt32(Console.ReadLine());
+Console.Write("Welche Zahl soll als Maximum gesetzt werden: ");
+end = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Wie viele Zahlen möchtest du testen: ");
+
+myNumbers = tester.produceArray(Convert.ToInt32(Console.ReadLine()));
+
+for (int i = 0; i < myNumbers.Length; i++)
+{
+    Console.Write("[{0}. Zahl]: ", i+1);
+    numberAdd = Convert.ToInt32(Console.ReadLine());
+
+    if (numberAdd >= start && numberAdd <= end)
+    {
+        if (Array.Exists(myNumbers, x => x == numberAdd))
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("FEHLER! Keine Doppelten Zahlen verwenden...");
+            Console.ForegroundColor = ConsoleColor.White;
+            i--;
+        }
+        else
+        {
+            myNumbers[i] = numberAdd;
+        }
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("FEHLER! Zahl muss im angegebenen Bereich liegen! [START: {0} | END: {1}]", start, end);
+        Console.ForegroundColor = ConsoleColor.White;
+        i--;
+    }
+
+}
+
+counter = tester.startLoop(start, end, myNumbers);
+spendingMoney = priceForOneTip * counter;
+weeks = counter / perWeek;
+months = weeks / 4;
+years = months / 12;
+
+Console.WriteLine("----------------------------------------");
+Console.WriteLine("Benöigte Versuche: {0}", counter);
+Console.WriteLine("----------------------------------------");
+Console.WriteLine("Preis pro Tipp: {0}", priceForOneTip);
+Console.WriteLine("Ausgegeben bis Hauptgewinn: {0}", spendingMoney);
+Console.WriteLine("Dauer bei {0} Ziehungen pro Woche: ", perWeek);
+Console.WriteLine("Wochen: {0}", weeks);
+Console.WriteLine("Monate: {0}", months);
+Console.WriteLine("Jahre: {0}", years);
+
---
+++ TestingClass.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zahlen_Raten
+{
+    internal class TestingClass
+    {
+        public int[] produceArray(int arraySize)
+        {
+            int[] array = new int[arraySize];
+            return array;
+        }
+
+        public bool checkArrays(int[] numbers, int[] randNumbers)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if(Array.Exists(randNumbers, x => x == numbers[i]))
+                {
+                    counter++;
+                }
+            }
+
+            if(counter == numbers.Length)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            
+        }
+
+        public int startLoop(int start, int stop, int[] numbers)
+        {
+            int randNumber;
+            int counter = 0;
+            int[] randArray = produceArray(numbers.Length);
+            bool won = false;
+            // Max-Wert ist exklusiv
+            Random random = new Random();
+
+            do
+            {
+                counter++;
+                Console.WriteLine("---------- ROUND {0} ----------", counter);
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    randNumber = random.Next(start, stop + 1);
+                    if (Array.Exists(randArray, x => x == randNumber))
+                    {
+                        while(Array.Exists(randArray, x => x == randNumber))
+                        {
+                            randNumber = random.Next(start, stop + 1);
+                        }
+                    }
+                    else
+                    {
+                        randArray[i] = randNumber;
+                    }
+                        Console.WriteLine("[{0}. Wert]: {1}", i + 1, randArray[i]);
+                }
+
+                won = checkArrays(numbers, randArray);
+                Console.WriteLine();
+
+            } while (won == false);
+
+            return counter;
+        }
+    }
+}
